259f676d82f90a0dfc7d853be637ec99b3065dc5
Steve83100 <2098064923@qq.com>
2026-01-17 11:31:44 +0800

    Complete CLIP zero-shot and proj-tune

--- 3_CLIP/TinyImageNet.py
+++ 3_CLIP/TinyImageNet.py
@@ -5,9 +5,9 @@ import random
 import shutil
 import time
 import torch
-import torchvision
 import torchvision.transforms as trans
-from torch.utils.data import Dataset, random_split, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
+from torchvision.datasets import ImageFolder
 
 
 
@@ -27,7 +27,7 @@ DATASET_PATH = "D:/Coding/DeepLearning/Datasets/"
 # and we don't want to do it every time we load the dataset.
 
 # if __name__ == "__main__":
-#     dataset = torchvision.datasets.ImageFolder(root=DATASET_PATH+"tiny-imagenet-200/train/")
+#     dataset = ImageFolder(root=DATASET_PATH+"tiny-imagenet-200/train/")
 #     all_classes = list(dataset.class_to_idx.keys())
 #     selected_classes = random.sample(all_classes, 50)
 #     print(selected_classes)
@@ -53,7 +53,7 @@ DATASET_PATH = "D:/Coding/DeepLearning/Datasets/"
 # Note that this might not be necessary, as CLIP has its own normalizing parameters in "preprocess"
 
 # if __name__ == "__main__":
-#     train_set = torchvision.datasets.ImageFolder(root=DATASET_PATH+"micro-imagenet-50/", transform=trans.ToTensor())
+#     train_set = ImageFolder(root=DATASET_PATH+"micro-imagenet-50/", transform=trans.ToTensor())
 #     print(len(train_set))
 #     loader = DataLoader(train_set, batch_size=64, shuffle=False, num_workers=4)
 
@@ -82,28 +82,54 @@ DATASET_PATH = "D:/Coding/DeepLearning/Datasets/"
 
 # ====================================================================
 
-# TODO: Split dataset and apply different transforms with my own implementation of ImageFolder
-
-class TinyImageNet(Dataset):
+class TinyImageNet():
     """
     TinyImageNet dataset with 50 classes and 25000 images, created from filtering tiny-imagenet-200.
+    Dataset is randomly split into training, validating, and testing set, according to given ratio.
     """
-    def __init__(self, additional_preprocess):
-        super().__init__()
+    
+    class DatasetCustomTransforms(Dataset):
+        """Applies custom transforms to dataset while loading data."""
+        def __init__(self, dataset, transform=None):
+            self.dataset = dataset
+            self.transform = transform
         
-        self.train = True # Dataset mode, decides 
+        def __getitem__(self, idx):
+            x, y = self.dataset[idx]
+            if self.transform:
+                x = self.transform(x)
+            return x, y
+        
+        def __len__(self):
+            return len(self.dataset)
+        
+    
+    def __init__(self, additional_preprocess, train_valid_test_ratio):
+        
+        # Load original dataset without transforms
+        print("Loading data...")
+        self.original_dataset = ImageFolder(root=DATASET_PATH+"micro-imagenet-50/")
+        print("Original dataset size:", len(self.original_dataset))
         
         # Define preprocessing methods
-        print("Processing data...")
-        self.train_transform = trans.Compose([ # No normalization since CLIP has its own
+        train_transform = trans.Compose([ # No normalization since CLIP has its own
             trans.RandomHorizontalFlip(),
             trans.RandomVerticalFlip(),
+            # Is RandomCrop suggested when CLIP itself performs a CenterCrop(224)?
             additional_preprocess
             ])
-        self.test_transform = additional_preprocess
+        test_transform = additional_preprocess
         
-        self.dataset = torchvision.datasets.ImageFolder(root=DATASET_PATH+"micro-imagenet-50/")
-        print("Dataset size:", len(self.dataset))
+        # Split dataset and apply separate transforms
+        # Using random_split will result in each class having different train/valid/test ratios then desired 400/50/50
+        # For example, one might have 350/60/40, another might have 450/20/30
+        # But this is relatively a minor issue with large, balanced datasets like ImageNet.
+        print("Split and transform data...")
+        train_set, valid_set, test_set = random_split(self.original_dataset, train_valid_test_ratio)
+        self.train_set = TinyImageNet.DatasetCustomTransforms(train_set, train_transform)
+        self.valid_set = TinyImageNet.DatasetCustomTransforms(valid_set, test_transform)
+        self.test_set = TinyImageNet.DatasetCustomTransforms(test_set, test_transform)
+        print(f"Resulting size: train {len(self.train_set)}, validate {len(self.valid_set)}, test {len(self.test_set)}")
         
         # Map class code to its name, such as "n03908204" to "pencil"
         self.code_name_dict = {}
@@ -113,27 +139,19 @@ class TinyImageNet(Dataset):
                 self.code_name_dict[split_line[0]] = split_line[1].strip()
         f.close()
 
-    def __getitem__(self, index):
-        image, label_id = self.dataset[index]
-        return image, label_id
-    
-    def __len__(self):
-        return len(self.dataset)
 
 
-
-def get_data(batch_size, train_valid_test_ratio, additional_preprocess):
+def get_data(batch_size, additional_preprocess, train_valid_test_ratio):
     """
     Get train, validate and test loaders of filtered tiny-imagenet-200.
         
         :param train_valid_test_ratio: List of int or float such as [0.8, 0.1, 0.1]
     
     """
-    dataset = TinyImageNet(additional_preprocess)
-    train_set, valid_set, test_set = random_split(dataset, train_valid_test_ratio)
-    train_loader = DataLoader(train_set, shuffle = True, batch_size = batch_size)
-    valid_loader = DataLoader(valid_set, shuffle = False, batch_size = batch_size)
-    test_loader = DataLoader(test_set, shuffle = False, batch_size = batch_size)
+    dataset = TinyImageNet(additional_preprocess, train_valid_test_ratio)
+    train_loader = DataLoader(dataset.train_set, shuffle = True, batch_size = batch_size)
+    valid_loader = DataLoader(dataset.valid_set, shuffle = False, batch_size = batch_size)
+    test_loader = DataLoader(dataset.test_set, shuffle = False, batch_size = batch_size)
     
     return dataset, train_loader, valid_loader, test_loader
 
@@ -145,18 +163,13 @@ def get_data(batch_size, train_valid_test_ratio, additional_preprocess):
 
 if __name__ == "__main__":
     model, preprocess = clip.load("ViT-B/32")
+    dataset, train_loader, valid_loader, test_loader = get_data(12, preprocess, [0.8, 0.1, 0.1])
     
-    dataset = TinyImageNet(preprocess)
-    image, label_id = dataset[499]
+    image, label_id = dataset.train_set[300]
     print(image.shape)
     print(label_id)
-    print(dataset.code_name_dict[dataset.dataset.classes[label_id]])
+    print(dataset.code_name_dict[dataset.original_dataset.classes[label_id]])
     
-    image, label_id = dataset[500]
-    print(label_id)
-    print(dataset.code_name_dict[dataset.dataset.classes[label_id]])
-    
-    dataset, train_loader, valid_loader, test_loader = get_data(12, [0.8, 0.1, 0.1], preprocess)
     images, label_ids = next(iter(train_loader))
     print(images.shape)
     print(label_ids)
--- 3_CLIP/main_training.py
+++ 3_CLIP/main_training.py
@@ -1,5 +1,7 @@
 from TinyImageNet import get_data
-
+import math
+from matplotlib import pyplot as plt
+import time
 import torch
 import torch.nn as nn
 import clip
@@ -10,10 +12,12 @@ import clip
 
 # Hyperparameters
 
+ROOT_PATH = "~/syang/ExaiPreparations/" # Path of ExaiPreparations
+MODEL_PATH = ROOT_PATH + "ModelSaves/"
+OUTPUT_PATH = ROOT_PATH + "Outputs/"
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 BATCH_SIZE = 12
-LR = 5e-5
-EPOCHS = 50
+EPOCHS = 3
 
 
 
@@ -34,8 +38,11 @@ model.to(DEVICE)
 # Define training and testing functions
 
 def train(loader, model, text_features, device, criterion, optimizer):
+    """Train model on given dataloader for 1 epoch. Returns average loss and accuracy."""
     model.train()
     total_loss = 0
+    total_correct = 0
+    total = 0
     optimizer.zero_grad()
     
     for images, label_ids in loader:
@@ -47,15 +54,23 @@ def train(loader, model, text_features, device, criterion, optimizer):
         loss = criterion(logits, label_ids) # label_ids: (batch_size)
         loss.backward()
         optimizer.step()
-        
         total_loss += loss.item()
         
-    return total_loss / len(loader)
+        _, preds = torch.max(logits, dim = 1) # Manually retrive predicted class index from outputs
+        total_correct += (preds == label_ids).sum().item() # Count the number of correct predictions
+        total += label_ids.size(0)
+        
+    accuracy = total_correct / total * 100
+        
+    return total_loss / len(loader), accuracy
 
 @torch.no_grad()
 def test(loader, model, text_features, device, criterion):
+    """Test model on given dataloader. Returns average loss and accuracy."""
     model.eval()
     total_loss = 0
+    total_correct = 0
+    total = 0
     
     for images, label_ids in loader:
         images = images.to(device)
@@ -66,7 +81,18 @@ def test(loader, model, text_features, device, criterion):
         loss = criterion(logits, label_ids) # label_ids: (batch_size)
         total_loss += loss.item()
         
-    return total_loss / len(loader)
+        _, preds = torch.max(logits, dim = 1) # Manually retrive predicted class index from outputs
+        total_correct += (preds == label_ids).sum().item() # Count the number of correct predictions
+        total += label_ids.size(0)
+        
+    accuracy = total_correct / total * 100
+        
+    return total_loss / len(loader), accuracy
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
 
 
 
@@ -74,15 +100,15 @@ def test(loader, model, text_features, device, criterion):
 
 # Get dataset and dataloaders
 
-dataset, train_loader, valid_loader, test_loader = get_data(BATCH_SIZE, [0.8, 0.1, 0.1], preprocess)
+dataset, train_loader, valid_loader, test_loader = get_data(BATCH_SIZE, preprocess, [0.8, 0.1, 0.1])
 
 
 
 # ====================================================================
 
-# Prompt, tokenize and calculate representations for all classes
+# For all class ids: map to natural language, create prompt, tokenize, calculate representations
 
-class_codes = dataset.dataset.classes
+class_codes = dataset.original_dataset.classes
 class_names = ["a photo of a " + dataset.code_name_dict[class_code] for class_code in class_codes]
 # print(class_names)
 class_tokens = clip.tokenize(class_names)
@@ -99,10 +125,107 @@ text_features = text_features / text_features.norm(dim=-1, keepdim=True)
 
 # 1. Zero-shot
 
-print(test(test_loader, model, text_features, DEVICE, nn.CrossEntropyLoss()))
+# print("Testing zero-shot performance...")
+# loss, accu = test(test_loader, model, text_features, DEVICE, nn.CrossEntropyLoss())
+# print("Zero-shot accuracy:", accu)
 
 
 
 # ====================================================================
 
-# 2. Fine-tune projection head
+# 2. Fine-tune image encoder's projection head
+
+print("Preparing to fine-tune image encoder's projection head...")
+
+# Freeze everything except image encoder's projection head
+for param in model.parameters():
+    param.requires_grad = False
+model.visual.proj.requires_grad = True
+model.logit_scale.requires_grad = True
+
+# Check parameters to fine-tune
+for name, param in model.named_parameters():
+    if param.requires_grad:
+        print("Trainable:", name)
+
+# Create optimizer on trainable parameters
+optimizer = torch.optim.AdamW(
+    filter(lambda p: p.requires_grad, model.parameters()),
+    lr=1e-4, # Bigger lr for projection head fine-tuning
+    weight_decay=1e-4
+)
+
+# Fine-tune
+print("Start fine-tuning projection head...")
+epochs = []
+train_losses = []
+train_accuracies = []
+valid_losses = []
+valid_accuracies = []
+criterion = nn.CrossEntropyLoss()
+start_time = time.time()
+
+for epoch in range(1, EPOCHS):
+    train_loss, train_accu = train(train_loader, model, text_features, DEVICE, criterion, optimizer)
+    valid_loss, valid_accu = test(valid_loader, model, text_features, DEVICE, criterion)
+    
+    time_spent = asMinutes(time.time() - start_time)
+    print(f"Epoch[{epoch}/{EPOCHS}]: Time spent:{time_spent}."
+          + f"\n\tTrain: loss {train_loss:.4f}, accuracy {train_accu:.4f}"
+          + f"\n\tValid: loss {valid_loss:.4f}, accuracy {valid_accu:.4f}")
+    
+    epochs.append(epoch)
+    train_losses.append(train_loss)
+    train_accuracies.append(train_accu)
+    valid_losses.append(valid_loss)
+    valid_accuracies.append(valid_accu)
+    
+# Plot graph
+print("Plotting loss and accuracy graph...")
+fig = plt.figure()
+
+loss_graph = fig.add_subplot(121)
+loss_graph.plot(epochs, train_losses, label = "Training Loss")
+loss_graph.plot(epochs, valid_losses, label = "Validation Loss")
+loss_graph.set_title("Loss")
+loss_graph.set_xlabel("Epoch")
+loss_graph.legend()
+
+accuracy_graph = fig.add_subplot(122)
+accuracy_graph.plot(epochs, train_accuracies, label = "Training Accuracy")
+accuracy_graph.plot(epochs, valid_accuracies, label = "Validation Accuracy")
+accuracy_graph.set_title("Accuracy")
+accuracy_graph.set_xlabel("Epoch")
+accuracy_graph.legend()
+
+# plt.savefig(OUTPUT_PATH + "CLIP-microImageNet50.png")
+plt.show()
+
+
+
+# ====================================================================
+
+# 3. Fine-tune image encoder's last K layers
+
+# K = 3
+# print("Preparing to fine-tune image encoder's last K layers...")
+
+# # Freeze everything except image encoder's last K layers
+# for param in model.parameters():
+#     param.requires_grad = False
+# blocks = model.visual.transformer.resblocks
+# for block in blocks[-K:]:
+#     for param in block.parameters():
+#         param.requires_grad = True
+# model.visual.proj.requires_grad = True
+# model.logit_scale.requires_grad = True
+# for name, param in model.named_parameters():
+#     if param.requires_grad:
+#         print("Trainable:", name)
+
+# Create optimizer on trainable parameters
+# optimizer = torch.optim.AdamW(
+#     filter(lambda p: p.requires_grad, model.parameters()),
+#     lr=1e-5, # Smaller lr for last-K fine-tuning
+#     weight_decay=1e-4
+# )
